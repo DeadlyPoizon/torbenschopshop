4e357a5761fba808ee2b29c1dfdbf87ebd9739da
poizo <wgj>
2022-10-06 11:05:23 +0200

    Initial commit

---
+++ src/main/java/com/chopshop/demo/TorbensChopShopApplication.java
@@ -0,0 +1,13 @@
+package com.chopshop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorbensChopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorbensChopShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chopshop/demo/TorbensChopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chopshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorbensChopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
